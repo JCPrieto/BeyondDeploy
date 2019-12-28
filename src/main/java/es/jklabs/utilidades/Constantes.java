0376d9e67abeb10ca5aecc9a42f3c90ba92f2fb2
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2019-12-28 21:36:13 +0100

    * 0.5.2
        * Actualización de seguridad de despendencias.

--- src/main/java/es/jklabs/utilidades/Constantes.java
+++ src/main/java/es/jklabs/utilidades/Constantes.java
@@ -4,7 +4,7 @@ public class Constantes {
 
     public static final String NOMBRE_APP = "BeyondDeploy";
     static final String STORAGE_BUCKET = "curriculum-a2a80.appspot.com";
-    public static final String VERSION = "0.5.1";
+    public static final String VERSION = "0.5.2";
 
     private Constantes() {
 
